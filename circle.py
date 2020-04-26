7478507ff037d5400a4c3d9438d39d8ff411fc03
Hiyabye <hiyabye0465@gmail.com>
2020-04-27 01:05:13 +0900

    Make circle smoother

--- circle.py
+++ circle.py
@@ -10,23 +10,30 @@ def CalculateCircle(size, centerX, centerY):
   up = int(centerY - size)
   down = int(centerY + size)
 
-  for i in range(left, centerX, 5): # Up Crust
-    j = centerY - math.sqrt(size ** 2 - (i - centerX) ** 2)
-    pyautogui.moveTo(i, j, 0.05)
-    print(str(i) + " " + str(j))
-    i+=5
-  for j in range(up, centerY, 5): # Right Crust
-    i = math.sqrt(size ** 2 - (j - centerY) ** 2) + centerX
-    pyautogui.moveTo(i, j, 0.05)
-    print(str(i) + " " + str(j))
-  for i in range(right, centerX, -5):  # Down Crust
-    j = math.sqrt(size ** 2 - (i - centerX) ** 2) + centerY
-    pyautogui.moveTo(i, j, 0.05)
-    print(str(i) + " " + str(j))
-  for j in range(down, centerY, -5): # Left Crust
-    i = centerX - math.sqrt(size ** 2 - (j - centerY) ** 2)
-    pyautogui.moveTo(i, j, 0.05)
-    print(str(i) + " " + str(j))
+  i = left
+  j = up
+
+  while True:
+    while i >= left and i <= centerX: # Up Crust
+      j = centerY - math.sqrt(size ** 2 - (i - centerX) ** 2)
+      pyautogui.moveTo(i, j, 0.05)
+      print(str(i) + " " + str(j))
+      i+=5
+    while j >= up and j <= centerY: # Right Crust
+      i = math.sqrt(size ** 2 - (j - centerY) ** 2) + centerX
+      pyautogui.moveTo(i, j, 0.05)
+      print(str(i) + " " + str(j))
+      j+=5
+    while i >= centerX and i <= right: # Down Crust
+      j = math.sqrt(size ** 2 - (i - centerX) ** 2) + centerY
+      pyautogui.moveTo(i, j, 0.05)
+      print(str(i) + " " + str(j))
+      i-=5
+    while j >= centerY and j <= down: # Left Crust
+      i = centerX - math.sqrt(size ** 2 - (j - centerY) ** 2)
+      pyautogui.moveTo(i, j, 0.05)
+      print(str(i) + " " + str(j))
+      j-=5
 
 # Get screen size
 screenWidth, screenHeight = pyautogui.size()
@@ -41,5 +48,4 @@ else:
 print("The max size recommended for you is " + str(maxSize))
 size = int(input("Enter the size of the circle: "))
 pyautogui.alert("The mouse will now move in a circle size of " + str(size) + ".\nTo stop press shift+f5.")
-while True:
-  CalculateCircle(size, centerX, centerY)
+CalculateCircle(size, centerX, centerY)
